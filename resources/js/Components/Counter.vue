e0ec815d152a8c112acffc8def03fc87f9d699e4
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2023-11-27 20:46:51 -0500

    feat: change chmod 777

---
+++ resources/js/Components/Counter.vue
@@ -0,0 +1,13 @@
+<template>
+    <button @click="count++">{{ count }}</button>
+</template>
+ 
+
+
+
+
+<script setup>
+import { ref } from "vue";
+
+const count = ref(1);
+</script>
---
+++ resources/js/Components/PostSubmitions.vue
@@ -0,0 +1,32 @@
+
+<template>
+    <div>
+
+        <button @click="addInput" type='button' class='break-inside bg-blue-600 rounded-xl p-4 mb-4 w-64'>
+            <div class='flex items-center space-x-4'>
+                <span class='text-base font-medium text-white'>Submit Another</span>
+            </div>
+        </button>
+
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            inputs: [{ value: '' }],
+            selected: null,
+            options: ['list', 'of', 'options']
+        };
+    },
+    methods: {
+        addInput(e) {
+            e.preventDefault();
+            this.inputs.push({ value: '' });
+        },
+    }
+};
+</script>

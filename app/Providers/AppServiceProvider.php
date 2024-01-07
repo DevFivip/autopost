4ca4db22409d271fcdcc4634a36ff7625f7bc9d1
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2024-01-06 21:55:38 -0500

    feat: nueva build

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 use TomatoPHP\TomatoAdmin\Facade\TomatoMenu;
 use TomatoPHP\TomatoAdmin\Services\Contracts\Menu;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191); 
         //
         TomatoMenu::groups([
             __('App') => true,
